d06eee349485df8e029c422955f124ebc4f69957
ew0xx <info.volkann@gmail.com>
2023-05-03 07:50:49 +0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+          alt="Netlifx-Logo"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
